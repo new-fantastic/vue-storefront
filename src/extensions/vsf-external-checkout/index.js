d175d0116f7d06f54642044727e904946677ac5b
Aleksander Grygier <alek@grygier.co>
2019-03-28 20:13:14 +0100

    Modified vsf-external-checkout redirect

--- src/extensions/vsf-external-checkout/index.js
+++ src/extensions/vsf-external-checkout/index.js
@@ -1,11 +1,17 @@
+import rootStore from '@vue-storefront/store'
+
 const EXTENSION_KEY = 'external-checkout'
 
 export default function (app, router, store, config) {
   const cmsUrl = config.externalCheckout.cmsUrl
 
   router.beforeEach((to, from, next) => {
-    if (to.name === 'checkout') {
-      window.location.replace(cmsUrl + '/vue/cart/sync/token/' + store.state.user.token + '/cart/' + store.state.cart.cartServerToken)
+    if (to.name.includes('checkout')) {
+      // cmsUrl ends with /
+      // langPath starts with / So I had to remove first char from that by substr(1)
+      const langPath = rootStore.state.storeView.url ? rootStore.state.storeView.url.substr(1) + '/' : ''
+
+      window.location.replace(cmsUrl + langPath + 'vue/cart/sync/token/' + store.state.user.token + '/cart/' + store.state.cart.cartServerToken)
     } else {
       next()
     }
