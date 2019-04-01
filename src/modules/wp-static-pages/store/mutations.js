7057b31f5345fa34244382d623a1df32a8870f77
Fifciuu <filip.jdrasik@gmail.com>
2019-04-01 19:00:12 +0200

    Converted static pages to dynamic ones

---
+++ src/modules/wp-static-pages/store/mutations.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+
+export const mutations = {
+  setContent(state, payload) {
+    // reactivity
+    Vue.set(state, 'content', payload)
+  },
+  setLayout(state, payload) {
+    state.layout = payload
+  }
+}
