8dc3a3d425b882dd401d02762c291bc7a875aaa2
Fifciuu <filip.jdrasik@gmail.com>
2019-04-18 14:04:26 +0200

    Disabled inmodule injecting GTM

--- src/modules/vsf-google-tag-manager/hooks/afterRegistration.ts
+++ src/modules/vsf-google-tag-manager/hooks/afterRegistration.ts
@@ -15,10 +15,10 @@ const injectJs = function(w,d,s,l,i) {
 }
 
 export function afterRegistration({ Vue, config, store, isServer }){
-  if (!isServer && config.googleTagManager && config.googleTagManager.id) {
-      if(!store.state['vsf-google-tag-manager'].registered) {
-        injectJs(window, document, 'script', 'dataLayer', config.googleTagManager.id);
-        store.commit('vsf-google-tag-manager/' + SET_STATUS, true)
-      }
-  }
+  // if (!isServer && config.googleTagManager && config.googleTagManager.id) {
+  //     if(!store.state['vsf-google-tag-manager'].registered) {
+  //       injectJs(window, document, 'script', 'dataLayer', config.googleTagManager.id);
+  //       store.commit('vsf-google-tag-manager/' + SET_STATUS, true)
+  //     }
+  // }
 }
