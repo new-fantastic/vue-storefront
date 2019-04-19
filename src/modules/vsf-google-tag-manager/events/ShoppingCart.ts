86ba31d10273b1646f5a4bb6a54b26263e64f46f
Fifciuu <filip.jdrasik@gmail.com>
2019-04-19 17:27:57 +0200

    Struggling with Remove from cart duplication

--- src/modules/vsf-google-tag-manager/events/ShoppingCart.ts
+++ src/modules/vsf-google-tag-manager/events/ShoppingCart.ts
@@ -4,17 +4,18 @@ import { ProductData } from '../types/ProductData'
 import productCategoryName from '../util/productCategoryName'
 import sizeIdToLabel from '../util/sizeIdToLabel'
 import rootStore from '@vue-storefront/store'
+import { Queue } from '../structures/queue'
 
 declare const dataLayer
 
-export default (currency): void => {
+export default (currency): void => { 
     let myDebounceOnAdd:(Function | null) = null 
     let myDebounceOnDelete:(Function | null) = null 
 
     // Product first time added to cart
     EventBus.$on('cart-before-add', product => {
         if(!myDebounceOnAdd) {
-            myDebounceOnAdd = debounce(() => {
+            myDebounceOnAdd = debounce((product) => {
                 if(!rootStore.state.ui.searchpanel) {
                   return
                 }
@@ -43,13 +44,14 @@ export default (currency): void => {
                   })
             }, 2000)
         }
-        myDebounceOnAdd(product.product)
+        myDebounceOnAdd(product)
     })
 
     EventBus.$on('cart-before-itemchanged', product => {
         if(!myDebounceOnAdd) {
-            myDebounceOnAdd = debounce(() => {
+            myDebounceOnAdd = debounce((product) => {
                 const pr = product.product || product.item
+                console.log('CHANGE', product)
 
                 let categoryName = productCategoryName(pr)
                  
@@ -76,14 +78,20 @@ export default (currency): void => {
         }
 
         if(myDebounceOnAdd && product.item.qty > 1 && product.item.prev_qty < product.item.qty) {
-            myDebounceOnAdd(product.product)
+            myDebounceOnAdd(product)
         } 
     })
 
+    let queue = new Queue<ProductData>()
+    let lastProducts = null
+
     EventBus.$on('cart-before-delete', product => {
+        lastProducts = product
         if(!myDebounceOnDelete) {
-            myDebounceOnDelete = debounce(() => {
+            myDebounceOnDelete = debounce((product) => {
+                console.log('delete', product)
                 const pr = product.items[0]
+
                 let categoryName = productCategoryName(pr)
                  
                   const productData: ProductData = {
@@ -96,19 +104,41 @@ export default (currency): void => {
                     category: categoryName
                   }
         
-                  dataLayer.push({
-                    'event': 'removeFromCart',
-                    'ecommerce': {
-                      'remove': {     
-                        'products': [productData]
-                      }
-                    }
-                  })
-            }, 1000)
+                  // dataLayer.push({
+                  //   'event': 'removeFromCart',
+                  //   'ecommerce': {
+                  //     'remove': {     
+                  //       'products': [productData]
+                  //     }
+                  //   }
+                  // })
+
+                  queue.push(productData)
+            }, 2000)
         }
 
-        if(myDebounceOnDelete){
-            myDebounceOnDelete(product.product)
+        myDebounceOnDelete(product)
+    })
+
+    EventBus.$on('cart-after-delete', products => {
+      console.log(lastProducts, products);
+      // if(myDebounceOnDelete) {
+        // if(JSON.stringify(lastProducts) === JSON.stringify(products)) {
+        //   return;
+        // }
+      console.log('looping')
+        for(let lpIndex in lastProducts) {
+          console.log('LP', lastProducts[lpIndex])
+          let present = false
+          for(let pIndex in products) {
+            console.log('P', products[pIndex])
+            if(lastProducts[lpIndex].id == products[pIndex].id) {
+              present = true
+              break;
+            }
+          }
+          console.log(lastProducts[lpIndex], 'present?: ', present)
         }
+      // }
     })
 }
