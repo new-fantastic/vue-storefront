873af61988eb093538296f1702df11142fa9111f
Aleksander Grygier <alek@grygier.co>
2019-05-11 14:06:07 +0200

    Updates

--- src/modules/wp-rest-content/types/WPRState.ts
+++
@@ -1,7 +0,0 @@
-export interface WPRState {
-    topNav: Object,
-    topAlert: Object,
-    bottomMenu: Object,
-    contentSlots: Object,
-    lang: String
-}
--- src/modules/wp-rest-content/util/GetLang.ts
+++
@@ -1,8 +0,0 @@
-import config from 'config'
-import { Route } from 'vue-router'
-
-export const getLangByRoute = (route: Route): string => {
-    return config.storeViews.mapStoreUrlsFor.some(
-            el => route.name.includes(`${el}-`)
-        ) ? 'en' : 'pl'
-}
