f9196f420c6931c77589299eb87053ac28d388c4
Fifciuu <filip.jdrasik@gmail.com>
2019-06-12 13:48:21 +0200

    External checkout TypeError fix

--- src/extensions/vsf-external-checkout/index.js
+++ src/extensions/vsf-external-checkout/index.js
@@ -6,7 +6,7 @@ export default function (app, router, store, config) {
   const cmsUrl = config.externalCheckout.cmsUrl
 
   router.beforeEach((to, from, next) => {
-    if (to.name.includes('checkout')) {
+    if ('name' in to && typeof to.name === 'string' && to.name.includes('checkout')) {
       // cmsUrl ends with /
       // langPath starts with / So I had to remove first char from that by substr(1)
       const langPath = rootStore.state.storeView.url ? rootStore.state.storeView.url.substr(1) + '/' : ''
