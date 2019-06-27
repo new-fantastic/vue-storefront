8d5e3ab34234c9c085ba9e572af6c8657c3252bb
Fifciuu <filip.jdrasik@gmail.com>
2019-06-27 19:31:35 +0200

    Hardcoded 1/2

--- src/modules/centralized-catalog-sales/store/actions.ts
+++ src/modules/centralized-catalog-sales/store/actions.ts
@@ -6,19 +6,13 @@ import axios from 'axios'
 import OAuth from 'oauth-1.0a'
 import crypto from 'crypto'
 
-const oauth = OAuth({
-  consumer: { 
-    key: 'nq81kv6upca1ri7yl50s8kgwy09vy7jv', 
-    secret: 'ftpiy8grb3tg5mpkp0uz3lhxwaj9cm1r' 
-  },
-  signature_method: 'HMAC-SHA1',
-  hash_function(base_string, key) {
-    return crypto
-        .createHmac('sha1', key)
-        .update(base_string)
-        .digest('base64')
-  }
-})
+// const oauth = OAuth({
+//   consumer: { 
+//     key: 'nq81kv6upca1ri7yl50s8kgwy09vy7jv', 
+//     secret: 'ftpiy8grb3tg5mpkp0uz3lhxwaj9cm1r' 
+//   },
+//   signature_method: 'HMAC-SHA1'
+// })
 
 export const actions: ActionTree<SalesState, any> = {
   
@@ -29,13 +23,13 @@ export const actions: ActionTree<SalesState, any> = {
     }
 
     const requestTargetIds = config.sales
-    const token = {
-      key: '82e46mr971j8jfc6u20ukiywx2qcs4gd',
-      secret: 'nkn04h215oqcvj4p8lcblhfofcufbnmm'
-    }
+    // const token = {
+    //   key: '82e46mr971j8jfc6u20ukiywx2qcs4gd',
+    //   secret: 'nkn04h215oqcvj4p8lcblhfofcufbnmm'
+    // }
 
     try {
-      const requests = []
+      // const requests = []
       // for (let id of requestTargetIds) {
       //   const request_data = {
       //     method: 'GET',
@@ -52,30 +46,20 @@ export const actions: ActionTree<SalesState, any> = {
       //   );
       // }
 
-      const request_data = {
-        method: 'GET',
-        url: `https://checkout.flynwetsuits.com/pln/rest/V1/salesRules/4`
-      }
-      // https://checkout.flynwetsuits.com/pln/rest/V1/salesRules/3/?
-      // oauth_consumer_key=nq81kv6upca1ri7yl50s8kgwy09vy7jv
-      // &oauth_token=82e46mr971j8jfc6u20ukiywx2qcs4gd
-      // &oauth_signature_method=HMAC-SHA1
-      // &oauth_timestamp=1561652533
-      // &oauth_nonce=AVdIR6&oauth_version=1.0
-      // &oauth_signature=9KV4arru+SnbZ+ZbhuIeyRwPT9g=
+      // const request_data = {
+      //   method: 'GET',
+      //   url: `https://checkout.flynwetsuits.com/pln/rest/V1/salesRules/4`
+      // }
 
-      const hd = oauth.toHeader(oauth.authorize(request_data, token));
-      hd.Authorization = hd.Authorization.replace(
-        'oauth_consumer_key="undefined",', 
-        'oauth_consumer_key="nq81kv6upca1ri7yl50s8kgwy09vy7jv", oauth_token="82e46mr971j8jfc6u20ukiywx2qcs4gd",')
+      // const hd = oauth.toHeader(oauth.authorize(request_data, token));
+      // hd.Authorization = hd.Authorization.replace('oauth_consumer_key="undefined"', 'oauth_consumer_key="nq81kv6upca1ri7yl50s8kgwy09vy7jv"')
 
-
-      await axios.get(request_data.url, {
-        headers: {
-          ...hd,
-          'Content-Type': 'application/json'
-        }
-      })
+      // await axios.get(request_data.url, {
+      //   headers: {
+      //     ...hd,
+      //     'Content-Type': 'application/json'
+      //   }
+      // })
 
       //oauth_consumer_key="undefined"
 
@@ -134,7 +118,167 @@ export const actions: ActionTree<SalesState, any> = {
     //     "simple_free_shipping": "0"
     // },]
 
-      // commit(types.CCS_STORE, response)
+      commit(types.CCS_STORE, [
+        {
+          "rule_id": 3,
+          "name": "catalog-30",
+          "store_labels": [],
+          "description": "",
+          "website_ids": [
+              1,
+              2
+          ],
+          "customer_group_ids": [
+              0,
+              1,
+              2,
+              3
+          ],
+          "from_date": "2019-06-27",
+          "to_date": "2019-07-11",
+          "uses_per_customer": 0,
+          "is_active": true,
+          "condition": {
+              "condition_type": "Magento\\SalesRule\\Model\\Rule\\Condition\\Combine",
+              "aggregator_type": "all",
+              "operator": null,
+              "value": "1"
+          },
+          "action_condition": {
+              "condition_type": "Magento\\SalesRule\\Model\\Rule\\Condition\\Product\\Combine",
+              "conditions": [
+                  {
+                      "condition_type": "Magento\\SalesRule\\Model\\Rule\\Condition\\Product",
+                      "operator": "==",
+                      "attribute_name": "category_ids",
+                      "value": "25, 21, 26, 22"
+                  }
+              ],
+              "aggregator_type": "all",
+              "operator": null,
+              "value": "1"
+          },
+          "stop_rules_processing": true,
+          "is_advanced": true,
+          "sort_order": 0,
+          "simple_action": "by_percent",
+          "discount_amount": 30,
+          "discount_step": 0,
+          "apply_to_shipping": false,
+          "times_used": 0,
+          "is_rss": true,
+          "coupon_type": "NO_COUPON",
+          "use_auto_generation": false,
+          "uses_per_coupon": 0,
+          "simple_free_shipping": "0"
+      },
+      {
+        "rule_id": 2,
+        "name": "FLYN50",
+        "store_labels": [
+            {
+                "store_id": 0,
+                "store_label": "FLYN50"
+            },
+            {
+                "store_id": 1,
+                "store_label": "FLYN50"
+            }
+        ],
+        "description": "50% zniżki na zakupy w naszym sklepie",
+        "website_ids": [
+            1
+        ],
+        "customer_group_ids": [
+            0,
+            1,
+            2,
+            3
+        ],
+        "from_date": "2018-11-26",
+        "to_date": "2019-01-15",
+        "uses_per_customer": 0,
+        "is_active": false,
+        "condition": {
+            "condition_type": "Magento\\SalesRule\\Model\\Rule\\Condition\\Combine",
+            "aggregator_type": "all",
+            "operator": null,
+            "value": "1"
+        },
+        "action_condition": {
+            "condition_type": "Magento\\SalesRule\\Model\\Rule\\Condition\\Product\\Combine",
+            "aggregator_type": "all",
+            "operator": null,
+            "value": "1"
+        },
+        "stop_rules_processing": false,
+        "is_advanced": true,
+        "sort_order": 0,
+        "simple_action": "by_percent",
+        "discount_amount": 50,
+        "discount_step": 0,
+        "apply_to_shipping": false,
+        "times_used": 46,
+        "is_rss": true,
+        "coupon_type": "SPECIFIC_COUPON",
+        "use_auto_generation": false,
+        "uses_per_coupon": 0,
+        "simple_free_shipping": "0"
+    },
+    {
+      "rule_id": 4,
+      "name": "catalog-60",
+      "store_labels": [],
+      "description": "",
+      "website_ids": [
+          1,
+          2
+      ],
+      "customer_group_ids": [
+          0,
+          1,
+          2,
+          3
+      ],
+      "from_date": "2019-06-27",
+      "to_date": "2019-06-30",
+      "uses_per_customer": 0,
+      "is_active": true,
+      "condition": {
+          "condition_type": "Magento\\SalesRule\\Model\\Rule\\Condition\\Combine",
+          "aggregator_type": "all",
+          "operator": null,
+          "value": "1"
+      },
+      "action_condition": {
+          "condition_type": "Magento\\SalesRule\\Model\\Rule\\Condition\\Product\\Combine",
+          "conditions": [
+              {
+                  "condition_type": "Magento\\SalesRule\\Model\\Rule\\Condition\\Product",
+                  "operator": "==",
+                  "attribute_name": "category_ids",
+                  "value": "18"
+              }
+          ],
+          "aggregator_type": "all",
+          "operator": null,
+          "value": "1"
+      },
+      "stop_rules_processing": false,
+      "is_advanced": true,
+      "sort_order": 0,
+      "simple_action": "by_percent",
+      "discount_amount": 60,
+      "discount_step": 0,
+      "apply_to_shipping": false,
+      "times_used": 0,
+      "is_rss": true,
+      "coupon_type": "NO_COUPON",
+      "use_auto_generation": false,
+      "uses_per_coupon": 0,
+      "simple_free_shipping": "0"
+  }
+      ])
 
     } catch (e) {
       console.error('Centralized Catalog Sales!')
--- src/modules/centralized-catalog-sales/store/mutations.ts
+++ src/modules/centralized-catalog-sales/store/mutations.ts
@@ -3,6 +3,18 @@ import * as types from './mutation-types'
 
 export const mutations: MutationTree<any> = {
   [types.CCS_STORE] (state, payload) {
-    state.sales = payload
+    state.sales = {}
+    for(let item of payload) {
+      if(item.name.indexOf('catalog') !== 0) {
+        continue;
+      }
+
+      for (let cId of item.action_condition.conditions[0].value.split(',')) {
+        state.sales = {
+          ...state.sales,
+          [Number(cId.trim())]: item.discount_amount
+        }
+      }
+    }
   }
 }
