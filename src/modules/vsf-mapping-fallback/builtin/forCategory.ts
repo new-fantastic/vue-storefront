ca9b59b92427a52dd09d756433b448a4ffe2a096
Fifciuu <filip.jdrasik@gmail.com>
2019-08-11 11:09:06 +0200

    Router url fallback for static pages

---
+++ src/modules/vsf-mapping-fallback/builtin/forCategory.ts
@@ -0,0 +1,22 @@
+import { removeStoreCodeFromRoute } from '@vue-storefront/core/lib/multistore'
+import { Payload } from '../types/Payload'
+import config from 'config'
+
+
+export const forCategory = async ({ dispatch }, { url }: Payload) => {
+  url = (removeStoreCodeFromRoute(url) as string)
+  
+  try {
+    const category = await dispatch('category/single', { key: 'url_path', value: url }, { root: true })
+    if (category !== null) {
+      return {
+        name: 'category',
+        params: {
+          slug: category.slug
+        }
+      }
+    }
+  } catch {
+    return undefined
+  }
+}
