c840f1c9323345ee322d40488fe86ef692ea8472
Fifciuu <filip.jdrasik@gmail.com>
2019-08-28 15:31:24 +0200

    Quickfix for newin

--- src/modules/extended-product/separateByColors.ts
+++ src/modules/extended-product/separateByColors.ts
@@ -1,54 +1,51 @@
-export const divideProduct = (product, generator = null, limit: number = Infinity, spread: number = -1) => {
-
+export const divideProduct = (
+  product,
+  generator = null,
+  limit: number = Infinity,
+  spread: number = -1
+) => {
   if (!product) {
     throw new Error('ProductColorTiles not received "product" props');
   }
 
-
-  if(!product.hasOwnProperty('configurable_options')) {
+  if (!product.hasOwnProperty("configurable_options")) {
     // No colors fallback
     return {
       ...product,
       photo_plan: generator ? generator.next().value : 1
-    }
+    };
   }
   const colorsObject = product.configurable_options.find(
     v => v.attribute_code === "color"
   );
-
 
   if (!colorsObject) {
     // No colors fallback
     return {
       ...product,
       photo_plan: generator ? generator.next().value : 1
-    }
+    };
   }
 
   // const colors = colorsObject.values.map(v => v.label);
-  const products = {}
+  const products = {};
   for (let curr of product.configurable_children) {
-
     const parts = curr.name.split(" / ");
-    parts.pop()
+    parts.pop();
     const name = parts.join(" / ");
 
-    if (!products.hasOwnProperty(name) && Object.keys(products).length < limit) {
-      const baseProduct = Object.assign({}, product);
-      baseProduct.name = name;
-      baseProduct.image = curr.image;
-      baseProduct.thumbnail = curr.thumbnail;
-      baseProduct.small_image = curr.small_image;
-      baseProduct.price = curr.price;
-      baseProduct.priceInclTax = curr.priceInclTax;
-      baseProduct.priceTax = curr.priceTax;
-      baseProduct.specialPriceInclTax = curr.specialPriceInclTax;
-      baseProduct.specialPriceTax = curr.specialPriceTax;
-      baseProduct.special_price = curr.special_price;
-      baseProduct.sku = curr.sku;
-      baseProduct.configurable_children = [curr];
-      baseProduct.configurable_options = [];
-      baseProduct.photo_plan = generator ? generator.next().value : 1
+    if (
+      !products.hasOwnProperty(name) &&
+      Object.keys(products).length < limit
+    ) {
+      const baseProduct = {
+        ...product,
+        ...curr,
+        configurable_children: [curr],
+        configurable_options: [],
+        photo_plan: generator ? generator.next().value : 1,
+        name
+      };
       products[name] = baseProduct;
     } else if (products.hasOwnProperty(name)) {
       products[name].configurable_children.push(curr);
@@ -57,20 +54,20 @@ export const divideProduct = (product, generator = null, limit: number = Infinit
   // return Object.values(products)
 
   if (spread === -1) {
-    return Object.values(products)
+    return Object.values(products);
   } else {
-    return Object.values(products).slice(spread * -1)
+    return Object.values(products).slice(spread * -1);
   }
-}
+};
 
-export const count = (product) => {
+export const count = product => {
   if (!product) {
     throw new Error('ProductColorTiles not received "product" props');
   }
 
-  if(!product.hasOwnProperty('configurable_options')) {
+  if (!product.hasOwnProperty("configurable_options")) {
     // No colors fallback
-    return 1
+    return 1;
   }
   const colorsObject = product.configurable_options.find(
     v => v.attribute_code === "color"
@@ -78,26 +75,22 @@ export const count = (product) => {
 
   if (!colorsObject) {
     // No colors fallback
-    return 1
+    return 1;
   }
 
   // const colors = colorsObject.values.map(v => v.label);
 
-  const products = product.configurable_children.reduce(
-    (total, curr) => {
+  const products = product.configurable_children.reduce((total, curr) => {
+    const parts = curr.name.split("/");
+    parts.pop();
+    const name = parts.join("/");
 
-        const parts = curr.name.split("/");
-        parts.pop()
-        const name = parts.join("/");
+    if (!total.hasOwnProperty(name)) {
+      total[name] = 1;
+    }
 
-        if (!total.hasOwnProperty(name)) {
-          total[name] = 1;
-        } 
- 
-      return total;
-    },
-    {}
-  );
+    return total;
+  }, {});
 
-  return Object.keys(products).length
-}
+  return Object.keys(products).length;
+};
