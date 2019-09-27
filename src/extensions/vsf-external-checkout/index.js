28f15192012f8d66ec96e0a80105716786f2b970
Łukasz <lukasz.jedrasik@icloud.com>
2019-09-27 10:40:03 +0200

    Added external checkout

---
+++ src/extensions/vsf-external-checkout/index.js
@@ -0,0 +1,23 @@
+import rootStore from '@vue-storefront/core/store';
+
+const EXTENSION_KEY = 'external-checkout'
+
+export default function (app, router, store, config) {
+  const cmsUrl = config.externalCheckout.cmsUrl
+
+  router.beforeEach((to, from, next) => {
+    if (to.name.includes('checkout')) {
+      // cmsUrl ends with /
+      // langPath starts with / So I had to remove first char from that by substr(1)
+      const langPath = rootStore.state.storeView.url ? rootStore.state.storeView.url.substr(1) + '/' : ''
+
+      window.location.replace(cmsUrl + langPath + 'vue/cart/sync/token/' + store.state.user.token + '/cart/' + store.state.cart.cartServerToken)
+    } else {
+      next()
+    }
+  })
+
+  return {
+    EXTENSION_KEY
+  }
+}
