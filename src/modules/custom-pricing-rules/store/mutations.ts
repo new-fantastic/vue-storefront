bde9912031246ffd805b398710bd58217731f6da
Fifciuu <filip.jdrasik@gmail.com>
2019-10-01 19:34:42 +0200

    Custom pricing rule only when is_active equals true

--- src/modules/custom-pricing-rules/store/mutations.ts
+++ src/modules/custom-pricing-rules/store/mutations.ts
@@ -5,25 +5,27 @@ import { currentStoreView } from '@vue-storefront/core/lib/multistore';
 import config from 'config';
 import GetCategoryIds from '../util/GetCategoryIds';
 
+const shouldUseRule = (rule): Boolean => {
+  if (rule.coupon_type !== 'NO_COUPON') return false;
+  // Is available?
+  const test1 =
+    (rule.from_date && new Date() >= new Date(rule.from_date)) || !rule.from_date;
+  const test2 =
+    (rule.to_date && new Date() <= new Date(rule.to_date)) || !rule.to_date;
+
+  // Is in proper storeView?
+  let test3 = !config.storeViews.multistore;
+  if (!test3) {
+    const { storeId } = currentStoreView();
+    test3 = rule.website_ids.includes(storeId);
+  }
+
+  return test1 && test2 && test3 && rule.is_active;
+}
+
 export const mutations: MutationTree<any> = {
   [types.SET_RULES](state, rules) {
-    const filteredRules = rules.filter(v => {
-      if (v.coupon_type !== 'NO_COUPON') return false;
-      // Is available?
-      const test1 =
-        (v.from_date && new Date() >= new Date(v.from_date)) || !v.from_date;
-      const test2 =
-        (v.to_date && new Date() <= new Date(v.to_date)) || !v.to_date;
-
-      // Is in proper storeView?
-      let test3 = !config.storeViews.multistore;
-      if (!test3) {
-        const { storeId } = currentStoreView();
-        test3 = v.website_ids.includes(storeId);
-      }
-
-      return test1 && test2 && test3;
-    });
+    const filteredRules = rules.filter(v => shouldUseRule(v));
 
     state.rules = [
       ...filteredRules.map(v =>
@@ -33,23 +35,7 @@ export const mutations: MutationTree<any> = {
   },
 
   [types.SET_CATEGORIES](state, rules) {
-    const filteredRules = rules.filter(v => {
-      if (v.coupon_type !== 'NO_COUPON') return false;
-      // Is available?
-      const test1 =
-        (v.from_date && new Date() >= new Date(v.from_date)) || !v.from_date;
-      const test2 =
-        (v.to_date && new Date() <= new Date(v.to_date)) || !v.to_date;
-
-      // Is in proper storeView?
-      let test3 = !config.storeViews.multistore;
-      if (!test3) {
-        const { storeId } = currentStoreView();
-        test3 = v.website_ids.includes(storeId);
-      }
-
-      return test1 && test2 && test3;
-    });
+    const filteredRules = rules.filter(v => shouldUseRule(v));
 
     const categories = [];
     for (let condition of filteredRules) {
